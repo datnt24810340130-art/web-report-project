2866c2824d275f266630b05dfd5bb7a6a1bf7483
datnt24810340130-art <datnt.24810340130@epu.edu.vn>
2026-06-06 14:30:19 +0700

    update register

--- js/login.js
+++ js/login.js
@@ -1,1 +1,19 @@
-console.log("Trang đăng nhập")
+function dangnhap(){
+
+    let email = document.getElementById("email").value.trim();
+    let password = document.getElementById("password").value.trim();
+
+    if(email === "" || password === ""){
+        alert("Vui lòng nhập đầy đủ thông tin!");
+        return;
+    }
+
+    if(email === "admin@gmail.com" && password === "123456"){
+        alert("Đăng nhập thành công!");
+
+        window.location.href = "index.html";
+    }
+    else{
+        alert("Sai tài khoản hoặc mật khẩu!");
+    }
+}
