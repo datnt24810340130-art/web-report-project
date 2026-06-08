3121e1451bcf1fe4a3f60c7ff75536160cd080c3
datnt24810340130-art <datnt.24810340130@epu.edu.vn>
2026-06-08 10:55:02 +0700

    update index

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("LaptopStore Website");
+function timKiemTour(){
+    let diaDiem = document.querySelector(
+        '.hop-tim-kiem input[type="text"]'
+    ).value;
+
+    if(diaDiem.trim() === ""){
+        alert("Vui lòng nhập điểm đến!");
+        return;
+    }
+
+    alert("Đang tìm tour đến: " + diaDiem);
+}
+
+document.getElementById("btnTimKiem")
+        .addEventListener("click", timKiemTour);
